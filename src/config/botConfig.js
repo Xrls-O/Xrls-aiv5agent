b0a59b175e97275b475a84e8364bec065409c213
Xrls-O <finnussy@gmail.com>
2025-01-16 01:00:28 -0500

    Create botConfig.js

---
+++ src/config/botConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    prefix: '!',
+    ownerID: 'YOUR_DISCORD_USER_ID',
+    defaultSettings: {
+        prefix: '!',
+        welcomeChannel: 'bienvenidas',
+        modLogChannel: 'registro-mod',
+        adminRole: 'admin',
+        modRole: 'moderador',
+        embedColor: '#0099ff'
+    }
