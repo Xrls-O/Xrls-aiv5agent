ea58c4e967c1edd547c1353c0f97447d5d48a241
Xrls-O <finnussy@gmail.com>
2025-01-16 00:52:43 -0500

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -14,3 +14,14 @@ module.exports = {
         next();
     },
     authorize: (roles) => {
+        return (req, res, next) => {
+            const userRole = req.headers['role'];
+
+            if (!roles.includes(userRole)) {
+                return res.status(403).send({ message: 'You do not have permission to perform this action.' });
+            }
+
+            next();
+        };
+    }
+};
